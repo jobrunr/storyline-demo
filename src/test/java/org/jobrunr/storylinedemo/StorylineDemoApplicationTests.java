413b82c24f095ad5d2fe4052a11cfae96ea29416
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-09-24 09:19:42 +0200

    initial commit Spring Boot project

---
+++ src/test/java/org/jobrunr/storylinedemo/StorylineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jobrunr.storylinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorylineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
