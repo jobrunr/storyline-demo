2c649b3074c759ffe413ab3503d7e9d684a29d21
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-11-12 13:55:00 +0100

    show ability to add custom metadata to jaeger tracing

--- demo-solution/src/main/java/org/jobrunr/storylinedemo/payment/PaymentService.java
+++ demo-solution/src/main/java/org/jobrunr/storylinedemo/payment/PaymentService.java
@@ -1,5 +1,6 @@
 package org.jobrunr.storylinedemo.payment;
 
+import io.opentelemetry.api.trace.Tracer;
 import org.jobrunr.jobs.annotations.Job;
 import org.jobrunr.jobs.annotations.Recurring;
 import org.jobrunr.scheduling.JobScheduler;
@@ -17,10 +18,12 @@ public class PaymentService {
     private final RestClient restClient;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PaymentService.class);
+    private final Tracer tracer;
 
-    public PaymentService(JobScheduler jobScheduler, RestClient.Builder restClientBuilder) {
+    public PaymentService(JobScheduler jobScheduler, RestClient.Builder restClientBuilder, Tracer tracer) {
         this.jobScheduler = jobScheduler;
         this.restClient = restClientBuilder.baseUrl("http://localhost:8089").build();
+        this.tracer = tracer;
     }
 
     @Recurring(cron = "0 3 * * *")
@@ -60,8 +63,17 @@ public class PaymentService {
     }
 
     public void exportPaymentToExternalSystem(Payment payment) {
-        var verified = this.restClient.get().uri("/verify").retrieve().body(String.class);
-        LOGGER.info("Exported and verified: {} - verified: {}", payment, verified);
+        // Optional: you can add custom trace metadata inside the job span or add a new span that will show up in Jaeger
+        var span = tracer.spanBuilder("exportPaymentToExternalSystem").startSpan();
+        span.setAttribute("payment.amount", payment.amount());
+        span.setAttribute("payment.description", payment.description());
+
+        try {
+            var verified = this.restClient.get().uri("/verify").retrieve().body(String.class);
+            LOGGER.info("Exported and verified: {} - verified: {}", payment, verified);
+        } finally {
+            span.end();
+        }
     }
 
 }
